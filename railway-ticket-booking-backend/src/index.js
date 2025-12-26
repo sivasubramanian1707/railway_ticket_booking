a11b5d0dec5b623c26d954e1bcc11a340898f186
Thangarajiv2006 <thangarajiv2006@gmail.com>
2025-12-26 20:04:37 +0530

    backend setup initialization

---
+++ railway-ticket-booking-backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+
+//configration
+const PORT = process.env.PORT || 4000;
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => console.log(`Database connected successfully!`))
+  .catch((error) => console.log(error.message));
+
+app.use("/public", express.static(path.join(__dirname, "uploads")));
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on ${process.env.PORT}`));
